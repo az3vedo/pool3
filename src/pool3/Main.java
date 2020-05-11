05380813cef3ef34866f0e05db196b91361375b1
Eduarda Oliveira <duda.oliveira.1503@gmail.com>
2020-05-10 21:04:11 -0300

    addUser

--- src/pool3/Main.java
+++ src/pool3/Main.java
@@ -1,13 +1,26 @@
 package pool3;
+
 import pool3.Cash;
-import java.util.Date;
+import java.util.*;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		ArrayList<Login> login = new ArrayList<Login>();  
+		addUser(scan, login);
+		System.out.println(login.get(0).getUser());
+	}
+	public static void addUser(Scanner scan, ArrayList<Login> login) {
+		System.out.print("\n User: ");
+		String user = scan.nextLine();
+		System.out.print("\n Password: ");
+		String password = scan.nextLine();	
+		Login l = new Login(user, password);
+		login.add(l);
+		}
+	public void checkLogin() {
 		
 	}
-	
-	
-	
+
 }
