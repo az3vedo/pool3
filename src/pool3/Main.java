befc76dfbb6d3e9708294be920125500081a06c0
Eduarda Oliveira <duda.oliveira.1503@gmail.com>
2020-05-10 21:39:16 -0300

    addUser

--- src/pool3/Main.java
+++ src/pool3/Main.java
@@ -1,9 +1,16 @@
 package pool3;
 
+<<<<<<< HEAD
+import pool3.Cash;
 import java.util.*;
+=======
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+>>>>>>> branch 'master' of https://github.com/az3vedo/pool3
 
 public class Main {
-
+<<<<<<< HEAD
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -20,6 +27,40 @@ public class Main {
 		login.add(l);
 		}
 	public void checkLogin() {
-	}
 		
+=======
+	public Report gerarRelatorio(String start, String end, ArrayList<Cash> list) {
+		Report newReport = new Report(start,end);
+		for (Cash item : list) {
+			newReport.addBalance(item.getAmount());
+		}
+		return newReport;
+>>>>>>> branch 'master' of https://github.com/az3vedo/pool3
+	}
+<<<<<<< HEAD
+
 }
+=======
+	
+	public static void main(String[] args) {
+		Cash cash = new Cash(dateParser("08-05-2020"), "conta de luz", "outgoing", 300.0);
+		ArrayList<Cash> money = new ArrayList<Cash>();
+		money.add(cash);
+		
+		System.out.println(cash.getDate());
+	}
+	
+	private static Date dateParser(String date_string) {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		Date date = null;
+		try {
+		date = (Date) formatter.parse(date_string);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		return date;
+	}
+	
+	
+}
+>>>>>>> branch 'master' of https://github.com/az3vedo/pool3
